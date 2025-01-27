25ad315cb600363ec7affefbfe952fdc6e2ed9b1
greggagne <ggagne@westminsteru.edu>
2025-01-27 10:27:02 -0700

    Added method prototypes

--- src/ThreeDimensionalPoint.java
+++ src/ThreeDimensionalPoint.java
@@ -4,5 +4,44 @@
 
 public class ThreeDimensionalPoint {
 
+    public ThreeDimensionalPoint() {
 
+    }
+
+    public ThreeDimensionalPoint(double x, double y, double z) {
+
+    }
+    public double getX() {
+        return 0;
+    }
+
+    public double getY() {
+        return 0;
+    }
+
+    public double getZ() {
+        return 0;
+    }
+    public double shiftX(double amount) {
+        return 0;
+    }
+
+    public double shiftY(double amount) {
+        return 0;
+    }
+    public double shiftZ(double amount) {
+        return 0;
+    }
+
+    public void rotateX(double theta) {
+
+    }
+
+    void rotateY(double theta) {
+
+    }
+
+    void rotateZ(double theta) {
+
+    }
 }
